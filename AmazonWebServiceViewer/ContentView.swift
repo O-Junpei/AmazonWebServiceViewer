db9055dce43de08083578a855d5a06aca927b900
小野 純平 <snorlax.chemist.and.jazz@gmail.com>
2019-12-12 15:35:36 +0900

    Initial Commit

---
+++ AmazonWebServiceViewer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AmazonWebServiceViewer
+//
+//  Created by 小野 純平 on 2019/12/12.
+//  Copyright © 2019 com.swiswiswift.aws-viewer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
