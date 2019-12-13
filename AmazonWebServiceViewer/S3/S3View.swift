c2da7fed10d89a8c77e260e26752fd6d7b0bedcc
小野 純平 <snorlax.chemist.and.jazz@gmail.com>
2019-12-13 18:39:24 +0900

    ListView を押すとAlertが出るようにした。

--- AmazonWebServiceViewer/S3/S3View.swift
+++ AmazonWebServiceViewer/S3/S3View.swift
@@ -2,10 +2,19 @@ import SwiftUI
 
 struct S3View: View {
     @ObservedObject(initialValue: S3ViewModel()) var viewModel: S3ViewModel
+    @State private var showingAlert = false
+    @State private var bucket: Bucket!
+    
     var body: some View {
-       
         List(viewModel.buckets) { bucket in
             Text(bucket.Name)
+                .onTapGesture {
+                    print(bucket.Name)
+                    self.bucket = bucket
+                    self.showingAlert = true
+            }
+        } .alert(isPresented: self.$showingAlert) {
+            Alert(title: Text("リソースの削除"), message: Text("\(bucket.Name)" + "Wear sunscreen"), dismissButton: .default(Text("Got it!")))
         }
     }
 }
